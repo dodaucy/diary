4b82d134023474c7e2942ce96cc33aec2fa0d763
XGamerGuide <xgamerguide.offiziell@gmail.com>
2022-09-21 22:52:38 +0200

    Add starlette http exception handler

--- main.py
+++ main.py
@@ -12,6 +12,7 @@
 from fastapi import FastAPI, Form, HTTPException, Request, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from config import config
 
@@ -21,6 +22,19 @@ app = FastAPI(openapi_url=None)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
+
+
+@app.exception_handler(StarletteHTTPException)
+async def starlette_http_exception(request: Request, exc: StarletteHTTPException):
+    return templates.TemplateResponse(
+        "error.html",
+        {
+            "request": request,
+            "status_code": exc.status_code,
+            "detail": exc.detail
+        },
+        exc.status_code
+    )
 
 
 @app.get("/")
