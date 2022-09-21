a2c1d10b27c050d3afaa387d519cafc4cd5bd8c7
XGamerGuide <xgamerguide.offiziell@gmail.com>
2022-09-21 22:44:06 +0200

    Add a passwort check

--- main.py
+++ main.py
@@ -9,9 +9,11 @@
 ######################################
 
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Form, HTTPException, Request, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from config import config
 
 
 app = FastAPI(openapi_url=None)
@@ -23,6 +25,21 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def index(request: Request):
+    return templates.TemplateResponse(
+        "login.html",
+        {
+            "request": request
+        }
+    )
+
+
+@app.post("/")
+def login(request: Request, password: str = Form(...)):
+    if password.strip() != config.password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect password"
+        )
     return templates.TemplateResponse(
         "index.html",
         {
