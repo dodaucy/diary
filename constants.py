d291a3d93ba57e9028570b5a0ae718e4257a0d73
XGamerGuide <xgamerguide.offiziell@gmail.com>
2022-09-22 21:21:43 +0200

    Add settings handler

---
+++ constants.py
@@ -0,0 +1,14 @@
+######################################
+#                                    #
+#               diary                #
+#                                    #
+#                MIT                 #
+#     Copyright (C) 2022 dodaucy     #
+#  https://github.com/dodaucy/diary  #
+#                                    #
+######################################
+
+
+DEFAULT_FONT_COLOR = "#fcb205"
+DEFAULT_BACKGROUND_COLOR = "#1c1c1c"
+DEFAULT_FONT_FAMILY = "sans-serif"
---
+++ settings.py
@@ -0,0 +1,52 @@
+######################################
+#                                    #
+#               diary                #
+#                                    #
+#                MIT                 #
+#     Copyright (C) 2022 dodaucy     #
+#  https://github.com/dodaucy/diary  #
+#                                    #
+######################################
+
+
+import constants
+from globals import db
+
+
+class Settings:
+    def __init__(self):
+        self.style = None
+
+    async def load(self) -> None:
+        """Load settings from database"""
+        fetched_settings = await db.fetch_one("SELECT * FROM settings")
+        if fetched_settings:
+            # Load settings from database
+            self.style = {
+                "font_color": fetched_settings["font_color"],
+                "background_color": fetched_settings["background_color"],
+                "font_family": fetched_settings["font_family"]
+            }
+        else:
+            # Load default settings
+            self.style = {
+                "font_color": constants.DEFAULT_FONT_COLOR,
+                "background_color": constants.DEFAULT_BACKGROUND_COLOR,
+                "font_family": constants.DEFAULT_FONT_FAMILY
+            }
+            await db.execute(
+                "INSERT INTO settings (font_color, background_color, font_family) VALUES (:font_color, :background_color, :font_family)",
+                self.style
+            )
+
+    async def update(self, font_color: str, background_color: str, font_family: str) -> None:
+        """Update settings in database"""
+        self.style = {
+            "font_color": font_color,
+            "background_color": background_color,
+            "font_family": font_family
+        }
+        await db.execute(
+            "UPDATE settings SET font_color = :font_color, background_color = :background_color, font_family = :font_family",
+            self.style
+        )
