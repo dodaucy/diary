67c86167096548ac4eace99de41e7b28cf2f7e07
XGamerGuide <xgamerguide.offiziell@gmail.com>
2022-09-22 15:40:14 +0200

    Add session syste,

---
+++ globals.py
@@ -0,0 +1,17 @@
+######################################
+#                                    #
+#               diary                #
+#                                    #
+#                MIT                 #
+#     Copyright (C) 2022 dodaucy     #
+#  https://github.com/dodaucy/diary  #
+#                                    #
+######################################
+
+
+from databases import Database
+
+from config import config
+
+
+db = Database(config.database)
--- main.py
+++ main.py
@@ -9,12 +9,15 @@
 ######################################
 
 
-from fastapi import FastAPI, Form, HTTPException, Request, status
+import os
+
+from fastapi import Cookie, FastAPI, Form, HTTPException, Request, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from config import config
+from globals import db
 
 
 app = FastAPI(openapi_url=None)
@@ -37,8 +40,27 @@ async def starlette_http_exception(request: Request, exc: StarletteHTTPException
     )
 
 
+@app.on_event("startup")
+async def startup():
+    await db.connect()
+
+
 @app.get("/")
-def index(request: Request):
+async def index(request: Request, token: str = Cookie(None)):
+    if token is not None:
+        fetched_token = await db.fetch_one(
+            "SELECT * FROM sessions WHERE token = :token",
+            {
+                "token": token
+            }
+        )
+        if fetched_token is not None:
+            return templates.TemplateResponse(
+                "index.html",
+                {
+                    "request": request
+                }
+            )
     return templates.TemplateResponse(
         "login.html",
         {
@@ -48,15 +70,31 @@ def index(request: Request):
 
 
 @app.post("/")
-def login(request: Request, password: str = Form(...)):
+async def login(request: Request, password: str = Form(...)):
+    # Check if the password is correct
     if password.strip() != config.password:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect password"
         )
-    return templates.TemplateResponse(
+    # Create a session
+    token = os.urandom(32).hex()
+    await db.execute(
+        "INSERT INTO sessions (token) VALUES (:token)",
+        {
+            "token": token
+        }
+    )
+    # Return the session token and login
+    response = templates.TemplateResponse(
         "index.html",
         {
             "request": request
         }
     )
+    response.set_cookie(
+        key="token",
+        value=token,
+        httponly=True
+    )
+    return response
