caf275d2d9d431fdb8555ca904cf31a24f911050
XGamerGuide <xgamerguide.offiziell@gmail.com>
2022-09-29 21:38:34 +0200

    Add time support for config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 from dotenv import load_dotenv
+from humanfriendly import parse_timespan
 
 
 # Load environment variables from config.env file
@@ -12,9 +13,22 @@ DATABASE: str = os.environ["DATABASE"]
 PASSWORD_HASH: str = os.environ["PASSWORD_HASH"]
 
 
+class Time:
+    def __init__(self, time_string: str):
+        self.seconds = parse_timespan(time_string)
+
+
 class Config:
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
-            self.__setattr__(key, value)
-            if key in os.environ:
-                self.__setattr__(key, type(value)(os.environ[key]))
+            if isinstance(value, Time):
+                self.__setattr__(key, value.seconds)
+                if key in os.environ:
+                    if os.environ[key].isdigit():
+                        self.__setattr__(key, int(os.environ[key]))
+                    else:
+                        self.__setattr__(key, parse_timespan(os.environ[key]))
+            else:
+                self.__setattr__(key, value)
+                if key in os.environ:
+                    self.__setattr__(key, type(value)(os.environ[key]))
