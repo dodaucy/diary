acd1b2516526983eb284a70d00e383527297c291
XGamerGuide <xgamerguide.offiziell@gmail.com>
2022-09-30 15:33:08 +0200

    Add bool support for config

--- config.py
+++ config.py
@@ -21,14 +21,26 @@ class Time:
 class Config:
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
+            # Set default values
+            self.__setattr__(key, value)
             if isinstance(value, Time):
                 self.__setattr__(key, value.seconds)
-                if key in os.environ:
+            # Set environment variables
+            if key in os.environ:
+                if isinstance(value, Time):
+                    # Time
                     if os.environ[key].isdigit():
                         self.__setattr__(key, int(os.environ[key]))
                     else:
                         self.__setattr__(key, parse_timespan(os.environ[key]))
-            else:
-                self.__setattr__(key, value)
-                if key in os.environ:
+                elif isinstance(value, bool):
+                    # Boolean
+                    if os.environ[key].upper() in ("YES", "Y", "TRUE", "1"):
+                        self.__setattr__(key, True)
+                    elif os.environ[key].upper() in ("NO", "N", "FALSE", "0"):
+                        self.__setattr__(key, False)
+                    else:
+                        raise ValueError(f"{repr(key)} is not a valid boolean")
+                else:
+                    # Other
                     self.__setattr__(key, type(value)(os.environ[key]))
