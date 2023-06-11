e9f203dbef6201886b9e957d68cf7726b035c559
XGamerGuide <xgamerguide.offiziell@gmail.com>
2023-06-11 04:47:08 +0200

    Simplify SQL

--- create_tables.sql
+++ create_tables.sql
@@ -1,41 +1,31 @@
 CREATE TABLE IF NOT EXISTS `answers` (
-    `days` MEDIUMINT(8) UNSIGNED NOT NULL,
-    `question_id` INT(10) UNSIGNED NOT NULL,
-    `value` TINYINT(3) UNSIGNED NOT NULL,
-    PRIMARY KEY (`days`, `question_id`) USING BTREE
-)
-COLLATE='utf8mb4_general_ci'
-;
+    `days` MEDIUMINT UNSIGNED NOT NULL,
+    `question_id` INT UNSIGNED NOT NULL,
+    `value` TINYINT UNSIGNED NOT NULL,
+    PRIMARY KEY (`days`, `question_id`)
+);
 CREATE TABLE IF NOT EXISTS `notes` (
-	`days` MEDIUMINT(8) UNSIGNED NOT NULL,
-	`notes` TEXT NOT NULL COLLATE 'utf8mb4_general_ci',
-	PRIMARY KEY (`days`) USING BTREE
-)
-COLLATE='utf8mb4_general_ci'
-;
+	`days` MEDIUMINT UNSIGNED NOT NULL,
+	`notes` TEXT NOT NULL,
+	PRIMARY KEY (`days`)
+);
 CREATE TABLE IF NOT EXISTS `questions` (
-	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`name` TINYTEXT NOT NULL COLLATE 'utf8mb4_general_ci',
-	`color` VARCHAR(7) NOT NULL COLLATE 'utf8mb4_general_ci',
-	PRIMARY KEY (`id`) USING BTREE
-)
-COLLATE='utf8mb4_general_ci'
-;
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`name` TINYTEXT NOT NULL,
+	`color` VARCHAR(7) NOT NULL,
+	PRIMARY KEY (`id`)
+);
 CREATE TABLE IF NOT EXISTS `sessions` (
-	`token` CHAR(64) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`last_request` INT(11) NOT NULL,
-	`created_at` INT(11) NOT NULL,
-	PRIMARY KEY (`token`) USING BTREE
-)
-COLLATE='utf8mb4_general_ci'
-;
+	`token` CHAR(64) NOT NULL,
+	`last_request` INT NOT NULL,
+	`created_at` INT NOT NULL,
+	PRIMARY KEY (`token`)
+);
 CREATE TABLE IF NOT EXISTS `settings` (
-	`font_color` VARCHAR(7) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`button_font_color` VARCHAR(7) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`primary_background_color` VARCHAR(7) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`secondary_background_color` VARCHAR(7) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`navbar_selected_item_color` VARCHAR(7) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`font_family` VARCHAR(32) NOT NULL COLLATE 'utf8mb4_general_ci'
-)
-COLLATE='utf8mb4_general_ci'
-;
+	`font_color` VARCHAR(7) NOT NULL,
+	`button_font_color` VARCHAR(7) NOT NULL,
+	`primary_background_color` VARCHAR(7) NOT NULL,
+	`secondary_background_color` VARCHAR(7) NOT NULL,
+	`navbar_selected_item_color` VARCHAR(7) NOT NULL,
+	`font_family` VARCHAR(32) NOT NULL
+);
